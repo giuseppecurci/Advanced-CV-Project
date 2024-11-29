8e232b55f3de396ac04fa57aeaa4d0ef6d6d6d59
giuseppecurci <giuseppecurci49@gmail.com>
2024-11-29 20:21:45 +0100

    Adding WeCLIP code to run experiments

---
+++ scripts/dist_clip_voc.py
@@ -0,0 +1,200 @@
+import argparse
+import datetime
+import logging
+import os
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+import random
+import sys
+sys.path.append(".")
+import numpy as np
+import torch
+import torch.nn.functional as F
+from omegaconf import OmegaConf
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard import SummaryWriter
+from tqdm import tqdm
+
+from datasets import voc
+from utils.losses import get_aff_loss
+from utils import evaluate
+from utils.AverageMeter import AverageMeter
+from utils.camutils import cams_to_affinity_label
+from utils.optimizer import PolyWarmupAdamW
+from WeCLIP_model.model_attn_aff_voc import WeCLIP
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--config",
+                    default='/your/path/WeCLIP/configs/voc_attn_reg.yaml',
+                    type=str,
+                    help="config")
+parser.add_argument("--seg_detach", action="store_true", help="detach seg")
+parser.add_argument("--work_dir", default=None, type=str, help="work_dir")
+parser.add_argument("--radius", default=8, type=int, help="radius")
+parser.add_argument("--crop_size", default=320, type=int, help="crop_size")
+
+
+def setup_seed(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.backends.cudnn.deterministic = True
+
+def setup_logger(filename='test.log'):
+    ## setup logger
+    logFormatter = logging.Formatter('%(asctime)s - %(filename)s - %(levelname)s: %(message)s')
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
+
+    fHandler = logging.FileHandler(filename, mode='w')
+    fHandler.setFormatter(logFormatter)
+    logger.addHandler(fHandler)
+
+    cHandler = logging.StreamHandler()
+    cHandler.setFormatter(logFormatter)
+    logger.addHandler(cHandler)
+
+
+def cal_eta(time0, cur_iter, total_iter):
+    time_now = datetime.datetime.now()
+    time_now = time_now.replace(microsecond=0)
+
+    scale = (total_iter-cur_iter) / float(cur_iter)
+    delta = (time_now - time0)
+    eta = (delta*scale)
+    time_fin = time_now + eta
+    eta = time_fin.replace(microsecond=0) - time_now
+    return str(delta), str(eta)
+
+
+def validate(model=None, data_loader=None, cfg=None):
+
+    preds, gts, cams, aff_gts = [], [], [], []
+    num = 1
+    seg_hist = np.zeros((21, 21))
+    cam_hist = np.zeros((21, 21))
+    for _, data in tqdm(enumerate(data_loader),
+                        total=len(data_loader), ncols=100, ascii=" >="):
+        name, inputs, labels, cls_label = data
+
+        inputs = inputs.cuda()
+        labels = labels.cuda()
+
+        segs, cam, attn_loss = model(inputs, name, 'val')
+
+        resized_segs = F.interpolate(segs, size=labels.shape[1:], mode='bilinear', align_corners=False)
+
+        preds += list(torch.argmax(resized_segs, dim=1).cpu().numpy().astype(np.int16))
+        cams += list(cam.cpu().numpy().astype(np.int16))
+        gts += list(labels.cpu().numpy().astype(np.int16))
+
+        num+=1
+
+        if num % 1000 ==0:
+            seg_hist, seg_score = evaluate.scores(gts, preds, seg_hist)
+            cam_hist, cam_score = evaluate.scores(gts, cams, cam_hist)
+            preds, gts, cams, aff_gts = [], [], [], []
+
+    seg_hist, seg_score = evaluate.scores(gts, preds, seg_hist)
+    cam_hist, cam_score = evaluate.scores(gts, cams, cam_hist)
+    model.train()
+    return seg_score, cam_score
+
+
+def get_seg_loss(pred, label, ignore_index=255):
+    bg_label = label.clone()
+    bg_label[label!=0] = ignore_index
+    bg_loss = F.cross_entropy(pred, bg_label.type(torch.long), ignore_index=ignore_index)
+    fg_label = label.clone()
+    fg_label[label==0] = ignore_index
+    fg_loss = F.cross_entropy(pred, fg_label.type(torch.long), ignore_index=ignore_index)
+
+    return (bg_loss + fg_loss) * 0.5
+
+
+def get_mask_by_radius(h=20, w=20, radius=8):
+    hw = h * w
+    mask  = np.zeros((hw, hw))
+    for i in range(hw):
+        _h = i // w
+        _w = i % w
+
+        _h0 = max(0, _h - radius)
+        _h1 = min(h, _h + radius+1)
+        _w0 = max(0, _w - radius)
+        _w1 = min(w, _w + radius+1)
+        segs = F.interpolate(segs, size=pseudo_label.shape[1:], mode='bilinear', align_corners=False)
+
+        fts_cam = cam.clone()
+
+            
+        aff_label = cams_to_affinity_label(fts_cam, mask=attn_mask, ignore_index=cfg.dataset.ignore_index)
+        attn_loss, pos_count, neg_count = get_aff_loss(attn_pred, aff_label)
+
+        seg_loss = get_seg_loss(segs, pseudo_label.type(torch.long), ignore_index=cfg.dataset.ignore_index)
+
+        loss = 1 * seg_loss + 0.1*attn_loss
+
+
+        avg_meter.add({'seg_loss': seg_loss.item(), 'attn_loss': attn_loss.item()})
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        
+        if (n_iter + 1) % cfg.train.log_iters == 0:
+            
+            delta, eta = cal_eta(time0, n_iter+1, cfg.train.max_iters)
+            cur_lr = optimizer.param_groups[0]['lr']
+
+            preds = torch.argmax(segs,dim=1).cpu().numpy().astype(np.int16)
+            gts = pseudo_label.cpu().numpy().astype(np.int16)
+
+            seg_mAcc = (preds==gts).sum()/preds.size
+
+
+            logging.info("Iter: %d; Elasped: %s; ETA: %s; LR: %.3e;, pseudo_seg_loss: %.4f, attn_loss: %.4f, pseudo_seg_mAcc: %.4f"%(n_iter+1, delta, eta, cur_lr, avg_meter.pop('seg_loss'), avg_meter.pop('attn_loss'), seg_mAcc))
+
+            writer.add_scalars('train/loss',  {"seg_loss": seg_loss.item(), "attn_loss": attn_loss.item()}, global_step=n_iter)
+
+        
+        if (n_iter + 1) % cfg.train.eval_iters == 0:
+            ckpt_name = os.path.join(cfg.work_dir.ckpt_dir, "WeCLIP_model_iter_%d.pth"%(n_iter+1))
+            logging.info('Validating...')
+            if (n_iter + 1) > 26000:
+                torch.save(WeCLIP_model.state_dict(), ckpt_name)
+            seg_score, cam_score = validate(model=WeCLIP_model, data_loader=val_loader, cfg=cfg)
+            logging.info("cams score:")
+            logging.info(cam_score)
+            logging.info("segs score:")
+            logging.info(seg_score)
+
+    return True
+
+
+if __name__ == "__main__":
+
+    args = parser.parse_args()
+    cfg = OmegaConf.load(args.config)
+    cfg.dataset.crop_size = args.crop_size
+
+    if args.work_dir is not None:
+        cfg.work_dir.dir = args.work_dir
+
+    timestamp = "{0:%Y-%m-%d-%H-%M}".format(datetime.datetime.now())
+
+    cfg.work_dir.ckpt_dir = os.path.join(cfg.work_dir.dir, cfg.work_dir.ckpt_dir, timestamp)
+    cfg.work_dir.pred_dir = os.path.join(cfg.work_dir.dir, cfg.work_dir.pred_dir)
+    cfg.work_dir.tb_logger_dir = os.path.join(cfg.work_dir.dir, cfg.work_dir.tb_logger_dir, timestamp)
+
+    os.makedirs(cfg.work_dir.ckpt_dir, exist_ok=True)
+    os.makedirs(cfg.work_dir.pred_dir, exist_ok=True)
+    os.makedirs(cfg.work_dir.tb_logger_dir, exist_ok=True)
+
+    setup_logger(filename=os.path.join(cfg.work_dir.dir, timestamp+'.log'))
+    logging.info('\nargs: %s' % args)
+    logging.info('\nconfigs: %s' % cfg)
+
+    setup_seed(1)
+    train(cfg=cfg)
